c7a7a1de632936450b1c7921ed86f441118a3736
8605lux <31935899+8605lux@users.noreply.github.com>
2017-11-02 02:06:09 -0500

    Initial commit

---
+++ HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Best Fishing
+//
+//  Created by Tom on 2017/10/18.
+//  Copyright © 2017年 Deitel and Associates , Inc. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+class HomeViewController: UITabBarController {
+    
+    @IBOutlet weak var bottomTabBar: UITabBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for item in bottomTabBar.items! {
+            let unselectedItem: NSDictionary = [NSForegroundColorAttributeName: UIColor.gray, NSFontAttributeName: UIFont.systemFont(ofSize: 18)]
+            let selectedItem: NSDictionary = [NSForegroundColorAttributeName: UIColor.darkGray, NSFontAttributeName: UIFont.systemFont(ofSize: 18)]
+            item.setTitleTextAttributes(unselectedItem as? [String : AnyObject], for: .normal)
+            item.setTitleTextAttributes(selectedItem as? [String : AnyObject], for: .selected)
+            
+        }
+    }
+    
+}
